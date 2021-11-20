77230da96c80f2537552dc4658d29791e13b1440
jpmoraess <moraesdeveloperweb@gmail.com>
2021-11-20 00:26:12 -0300

    Initial commit

---
+++ src/main/java/br/com/moraesit/springrsocket/SpringRsocketApplication.java
@@ -0,0 +1,13 @@
+package br.com.moraesit.springrsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/moraesit/springrsocket/SpringRsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moraesit.springrsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
